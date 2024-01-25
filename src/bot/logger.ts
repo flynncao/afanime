fc72870da32ba0a466d189223ad82ca3ee3f1a0f
flynncao <flynncao@yahoo.com>
2024-01-25 12:49:55 +0800

    fix: mongodb connection error

--- src/bot/logger.ts
+++ src/bot/logger.ts
@@ -5,7 +5,7 @@ export default class BotLogger {
   static sendServerMessage = (message: string, otherConfig?: any): void => {
     const { bot } = store
     if (bot && process.env.USER_CHAT_ID)
-      bot.api.sendMessage(process.env.USER_CHAT_ID, otherConfig)
+      bot.api.sendMessage(process.env.USER_CHAT_ID, message, otherConfig)
 
     else
       Logger.logError('FATAL: Bot is not initialized or env file not found, cannot send message')
