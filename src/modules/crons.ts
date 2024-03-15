2129d45cbd08262601e6401fc450d5943c1262b2
flynncao <flynncao@yahoo.com>
2024-03-15 22:23:57 +0800

    fix: clear pushCenter list after pushing to channel

--- src/modules/crons.ts
+++ src/modules/crons.ts
@@ -34,6 +34,7 @@ export function initCrons() {
                     })
                   }
                 }
+                store.pushCenter.list = []
               }
             }
           })
