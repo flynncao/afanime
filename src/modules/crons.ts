42ee7e1afac610820a245245e1aaf4cc821632f3
flynncao <flynncao@yahoo.com>
2024-04-09 17:15:17 +0800

    fix: cron jobs reinitialization

--- src/modules/crons.ts
+++ src/modules/crons.ts
@@ -1,14 +1,14 @@
 import { CronJob } from 'cron'
-import store from '#root/databases/store.js'
-import Logger from '#root/utils/logger.js'
 import 'dotenv/config'
-import { fetchAndUpdateAnimeEpisodesInfo, readAnimes, updateAnimeMetaAndEpisodes } from '#root/models/Anime.js'
+import { executeAnimeEpisodeInfoTask } from './anime/task.js'
+import { fetchAndUpdateAnimeMetaInfo } from './anime/index.js'
+import Logger from '#root/utils/logger.js'
+
+import { readAnimes } from '#root/models/Anime.js'
 import BotLogger from '#root/bot/logger.js'
 
 export function initCrons() {
   try {
-    const botInstance = store.bot!
-    const userChatId = process.env.USER_CHAT_ID!
     const timeZone = 'Asia/Shanghai'
 
     const job = new CronJob('*/5 * * * * *', () => {
@@ -17,26 +17,10 @@ export function initCrons() {
 
     const dailyJob = new CronJob('0 0 8 * * *', () => {
       console.log('running a task every 8:00')
-      botInstance.api.sendMessage(userChatId, '早上好~现在开始推送动画更新').then(() => {
+      BotLogger.sendServerMessageAsync('早上好~现在开始推送动画更新').then(() => {
         readAnimes().then((res) => {
           res.filter(item => item.status === 1).forEach(async (anime) => {
-            const res = await fetchAndUpdateAnimeEpisodesInfo(anime.id)
-            if (typeof res === 'string') {
-              await BotLogger.sendServerMessageAsync(res)
-              if (store.pushCenter.list.length > 0) {
-                const list = store.pushCenter.list
-                for (const item of list) {
-                  if (item.link && item.link !== '') {
-                    const videoLink = item.link
-                    const episodePageLink = `https://bangumi.tv/ep/${item.bangumiID}`
-                    await BotLogger.sendServerMessageAsync(`原视频：${videoLink}\n评论区：${episodePageLink}`, {
-                      message_thread_id: store.pushCenter.threadID,
-                    })
-                  }
-                }
-                store.pushCenter.list = []
-              }
-            }
+            await executeAnimeEpisodeInfoTask(anime.id, anime.name_cn)
           })
         })
       })
@@ -44,11 +28,11 @@ export function initCrons() {
 
     const weeklyJob = new CronJob('0 0 0 * * 1', () => {
       console.log('running a task every Monday 0:00:00')
-      botInstance.api.sendMessage(userChatId, '新的一周开始了！~').then(() => {
+      BotLogger.sendServerMessageAsync('新的一周开始了！~拉取新的推送日程').then(() => {
         readAnimes().then((res) => {
           res.forEach(async (anime) => {
-            const msg = await updateAnimeMetaAndEpisodes(anime.id)
-            BotLogger.sendServerMessage(msg)
+            const msg = await fetchAndUpdateAnimeMetaInfo(anime.id)
+            BotLogger.sendServerMessageAsync(msg)
           })
         })
       })
